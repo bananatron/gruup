020ef7ee5f41d887eb48b90650cc2371ecbef097
bananatron <bananatrongo@gmail.com>
2015-02-21 04:39:34 +0000

    SO many fixes

--- routes.rb
+++ routes.rb
@@ -28,6 +28,19 @@ get '/submit' do
   erb :submit
 end
 
+get '/edit/:guid' do
+  @guid = params[:guid]
+  r_hash = $fb_apps.get("/"+@guid).body.first[1]
+  @app_name = r_hash["app_name"]
+  @app_description = r_hash["app_description"]
+  @get_link = r_hash["get_link"]
+  @version = r_hash["version"]
+  @link_description = r_hash["link_description"]
+  
+  erb :edit  
+end
+
+
 get '/guid/:guid/json' do
   content_type :json
   @guid = params[:guid]
@@ -63,7 +76,7 @@ get '/guid/:guid/text/:del' do
   app_name + del + app_description + del + get_link + del + version + del + link_description
 end
 
-post '/submit' do
+post '/submit/:is_edit' do
   @_id = params[:_id]
   @app_name = params[:app_name]
   @app_description = params[:app_description]
@@ -71,10 +84,15 @@ post '/submit' do
   @version = params[:version]
   @get_link = params[:get_link]
   @link_description = params[:link_description]
+  is_edit = params[:is_edit]
+  
+  $fb_apps.delete(@guid) if is_edit #delete entry if editing, then repush
+
   $fb_apps.push(@guid, :_id => @id, :app_name => @app_name, :app_description => @app_description, :guid => @guid, :version => @version, :get_link => @get_link, :link_description => @link_description)
   
   redirect to('/apps')
 end
+
 
 
 
